ecb948c8e5c1f269e153d9f0085b583749a7951f
liufeipekingu <liufeipekingu@4d5dd0c0-524b-11de-9eea-55519c9c5eb7>
2009-06-06 05:05:26 +0000

    REFACTOR:

---
+++ webservice-confidence/src/java/pku/sei/webservice/confidence/FileUtil.java
@@ -0,0 +1,22 @@
+package pku.sei.webservice.confidence;
+
+import java.io.*;
+import java.util.*;
+
+public class FileUtil {
+	public static List<List<String>> readFile(String file) throws IOException {
+		List<List<String>> result = new ArrayList<List<String>> ();
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			StringTokenizer st = new StringTokenizer(line);
+			List<String> array = new ArrayList<String> ();
+			while (st.hasMoreTokens()) {
+				array.add(st.nextToken());
+			}
+			result.add(array);
+		}
+		reader.close();
+		return result;
+	}
+}
---
+++ webservice-confidence/src/java/pku/sei/webservice/confidence/WsdlFile.java
@@ -0,0 +1,21 @@
+package pku.sei.webservice.confidence;
+
+import java.util.ArrayList;
+
+public class WsdlFile {
+	//TODO implement it
+	public static ArrayList<String> getWSDLEndpoints(String file) throws Exception {
+		ArrayList<String> endpoints = new ArrayList<String>();
+		endpoints.add("google.com");
+		return endpoints;
+	}
+	
+	public static String getDomain(String url) {
+		int pos = url.indexOf("://") + 3;
+		int end = url.indexOf("/", pos);
+		if (end == -1)
+			return url.substring(pos);
+		else
+			return url.substring(pos, end);
+	}
+}
